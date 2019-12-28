f82f5c8e3531c2754928fb564eef6b63f0690a73
pedrohrb <developer.pedro@hotmail.com>
2019-12-27 21:15:17 -0300

    ..

--- src/routes/home/routes.js
+++ src/routes/home/routes.js
@@ -11,17 +11,17 @@ const Users = mongoose.model('users')
 home.get('/',(req,res) => {
     res.json({ status: 'Ok' })
 })
-home.get('/users',async (req,res) => {
+home.get('/posts',async (req,res) => {
     const users = await Users.find()
 
     return res.json(users)
 })
-home.get('/oneuser/:id',async (req,res) => {
+home.get('/onepost/:id',async (req,res) => {
     const user = await Users.findById(req.params.id)
 
     return res.json(user)
 })
-home.post('/adduser',async (req,res) => {
+home.post('/addpost',async (req,res) => {
     const adduser = await Users.create(req.body)
 
     return res.json(adduser)
